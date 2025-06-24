501aab1a87ef04914cc3ec49899acb25240ef05f
baburhussain660 <baburhussain660@gmail.com>
2025-06-24 20:05:30 +0530

    Chore: Remove lingering Instamojo hook export for clean build

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
 // Export all hooks for easy importing
 export { useAuth } from './useAuth';
 export { useSubscription } from './useSubscription';
-export { usePhonePePayment } from './usePhonePePayment';
-export * from './useInstamojoPayment';
